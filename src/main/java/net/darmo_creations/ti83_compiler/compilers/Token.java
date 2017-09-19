a6c6730c903ad624fa582eca2c8fe6f698c8b384
Damien Vergnet <darmo117@gmail.com>
2017-09-19 19:31:27 +0200

    doc: Token class

--- src/main/java/net/darmo_creations/ti83_compiler/compilers/Token.java
+++ src/main/java/net/darmo_creations/ti83_compiler/compilers/Token.java
@@ -1,15 +1,33 @@
 package net.darmo_creations.ti83_compiler.compilers;
 
+/**
+ * This class represents an instruction token.
+ *
+ * @author Damien Vergnet
+ */
 public class Token {
   private String instruction;
   private String language;
   private byte[] token;
 
-  public Token(String instruction, byte[] token) {
+  /**
+   * Creates an unlocalized token.
+   * 
+   * @param instruction the source instruction
+   * @param token the token (can be several bytes)
+   */
+  public Token(String instruction, byte... token) {
     this(instruction, null, token);
   }
 
-  public Token(String instruction, String language, byte[] token) {
+  /**
+   * Creates a localized token.
+   * 
+   * @param instruction the source instruction
+   * @param language the language
+   * @param token the byte token
+   */
+  public Token(String instruction, String language, byte... token) {
     this.instruction = instruction;
     this.language = language;
     this.token = token;
