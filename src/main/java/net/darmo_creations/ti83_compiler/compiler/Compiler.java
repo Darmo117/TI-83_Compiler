d3f4771758a9e93a72fdb5e339e0645135398c27
Darmo117 <darmo117@gmail.com>
2022-03-13 20:54:00 +0100

    update version -> 2.0

--- src/main/java/net/darmo_creations/ti83_compiler/compiler/Compiler.java
+++ src/main/java/net/darmo_creations/ti83_compiler/compiler/Compiler.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
  * This class defines methods to compile source files into .8xp binary files and vice-versa.
  */
 public final class Compiler {
-  public static final String VERSION = "1.3";
+  public static final String VERSION = "2.0";
   public static final String BASE_SOURCE_EXTENSION = "ti83";
   public static final int DEFAULT_INDENT_SIZE = 2;
 
